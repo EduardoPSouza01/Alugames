f6bb8b8ee3b6ed264ff5cfff9609c9da2dee84d2
Eduardo Souza <eduardo.p.p.souza@hotmail.com>
2024-05-01 18:00:37 -0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
@@ -6,6 +12,7 @@ function alterarStatus(id){
 
     if(imagem.classList.contains('dashboard__item__img--rented')){
         alugarJogo(imagem, botao);
+        conformacao_devolucao();
     }else{
         devolverJogo(imagem, botao);
     }
@@ -16,11 +23,22 @@ function alugarJogo(imagem, botao){
     imagem.classList.remove('dashboard__item__img--rented');
     botao.classList.remove('dashboard__item__button--return');
     botao.innerHTML = "Alugar"; 
+    jogosAlugados--;
 }
 
 function devolverJogo(imagem, botao){
     imagem.classList.add('dashboard__item__img--rented');
     botao.classList.add('dashboard__item__button--return');
     botao.innerHTML = "Devolver";
+    jogosAlugados++;
 }
 
+function conformacao_devolucao(){
+    alert("Obrigado por devolver o jogo!!!!");
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
